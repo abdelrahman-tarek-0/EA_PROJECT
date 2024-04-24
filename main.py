3a2bf29ea6f34270a6a8978fc004405e2e677ca0
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-04-24 22:04:45 +0200

    Refactor main.py: Update main function to use Model class for data loading and fitting

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-form 
-from de_algo import DE
-
-
+from model import Model
+import numpy as np
 
 def main():
-    print(load_data('./dataset/'))
-  
+    model = Model.create_model(0.1)
+    
+    flattened_weights = np.concatenate([w.flatten() for w in model.get_weights()])
+    # print(flattened_weights)
+    flattened_weights = np.append(flattened_weights, 0.1)
+
+    data = Model.load_data("./dataset/")
+
+    Model.fit_function(flattened_weights, data)
+   
 
 if __name__ == '__main__':
-
     main()
